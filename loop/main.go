0dfc85872e14bc5e3570e7187203bf211a16cf63
Pete Hecht <peteehecht@gmail.com>
2017-11-30 12:41:05 -0600

    Updating main.go

--- loop/main.go
+++ loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for iterator := 32; iterator < 123; iterator++ {
+	for iterator := 32; iterator < 20000; iterator++ {
 		fmt.Printf("%d \t %b \t %x \t %q \n", iterator, iterator, iterator, iterator)
 	}
 }
