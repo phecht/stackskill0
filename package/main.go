b1d2d874ca97c2c42cd3ba32d62085ef6590c9fe
Pete Hecht <peteehecht@gmail.com>
2017-12-01 23:04:23 -0600

    Basic package setup.

---
+++ package/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stackskill0/package/visible"
+)
+
+func main() {
+	fmt.Println("This is from name.go in visible")
+	fmt.Printf("%v\n", visible.SecondName)
+}
---
+++ package/visible/name.go
@@ -0,0 +1,16 @@
+package visible
+
+// MyName is exported because it starts with a capital letter
+var MyName = "Todd"
+
+// FirstName is exported Paul.
+var FirstName = "Paul"
+
+// SecondName is exported John.
+var SecondName = "John"
+
+// ThirdName is exported George.
+var ThirdName = "George"
+
+// FourthName is exported Ringo
+var FourthName = "Ringo"
