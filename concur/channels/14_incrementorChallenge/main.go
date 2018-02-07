7564590c44a2c1e86df8516c294a1693e9bcdcc9
Pete Hecht <peteehecht@gmail.com>
2018-02-07 14:49:53 -0600

    Making incrementor work with channels.

---
+++ concur/channels/14_incrementorChallenge/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//var count int64
+//var wg sync.WaitGroup
+
+func main() {
+	//wg.Add(2)
+	inc1 := incrementor(5)
+	var count int
+	for counter := range inc1 {
+		count++
+		fmt.Println(counter)
+	}
+	fmt.Println("Final Counter:", count)
+
+}
+
+/* 	count := merge(inc1)
+}
+
+func merge(cs ...<-chan string) int {
+	count := 0
+	go func() {
+		for _, c := range cs {
+			count++
+			fmt.Println(c)
+		}
+
+	}()
+	return count
+*/
+
+func incrementor(incQty int) <-chan string {
+	out := make(chan string)
+	//defer close(out)
+	done := make(chan bool)
+
+	for i := 0; i < incQty; i++ {
+		go func(i int) {
+			for j := 0; j < 20; j++ {
+				//atomic.AddInt64(&count, 1)
+				outString := "Process: " + strconv.Itoa(i) + " printing:" + strconv.Itoa(j)
+				out <- outString
+			}
+			done <- true
+
+		}(i)
+
+	}
+	go func() {
+		for i := 0; i < incQty; i++ {
+			<-done
+		}
+		close(out)
+	}()
+	return out
+	//wg.Done()
+}
+
+/*
+CHALLENGE #1
+-- Take the code from above and change it to use channels instead of wait groups and atomicity
+*/
